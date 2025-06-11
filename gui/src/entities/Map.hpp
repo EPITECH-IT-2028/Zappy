93dd8996b6b932e28e7c47559cb469dc8e22228d
Chassagne <robin.chassagne@epitech.eu>
2025-06-11 18:47:49 +0200

    feat: map structure

---
+++ gui/src/entities/Map.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <stdexcept>
+#include <vector>
+#include "Tile.hpp"
+
+namespace gui {
+  struct Map {  
+    std::size_t width;
+    std::size_t height;
+    std::vector<std::vector<Tile>> tiles;
+
+    Map(std::size_t w, std::size_t h) : width(w), height(h), tiles(h, std::vector<Tile>(w)) {}
+
+  Tile& getTile(std::size_t x, std::size_t y) {
+    if (x >= width || y >= height)
+      throw std::out_of_range("Tile coordinates out of map\n");
+    return tiles[x][y];
+    }
+
+  const Tile& getTile(std::size_t x, std::size_t y) const {
+    return tiles[x][y];
+    }
+  };
+}
