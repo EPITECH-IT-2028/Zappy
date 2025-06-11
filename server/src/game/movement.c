c0b2e58f3bdb4b895cc15ed28cf8a44d04787abb
MelmanC <nolann.dubos@epitech.eu>
2025-06-11 20:35:03 +0200

    Server/Player can now spawn at an egg (#62)
    * feat: Player can now spawn on an eggs
    
    * fix: useless var
    
    * fix: Update player positions on the map
    
    * feat: Add cd to Forward, Left, Right

--- server/src/game/movement.c
+++ server/src/game/movement.c
@@ -5,6 +5,7 @@
 ** movement.c
 */
 
+#include "macro.h"
 #include "server.h"
 
 static
@@ -30,16 +31,9 @@ void set_offset(direction_offset_t *offset, client_t *client)
     }
 }
 
-int move_forward(server_t *server, response_t *response, request_t *request)
+static
+void check_map_bounds(server_t *server, client_t *client)
 {
-    client_t *client = request->client;
-    direction_offset_t offset;
-
-    if (!server || !response || !request || !client)
-        return ERROR;
-    set_offset(&offset, client);
-    client->data.x += offset.x;
-    client->data.y += offset.y;
     if (client->data.x < 0)
         client->data.x = server->params.width - 1;
     else if (client->data.x >= server->params.width)
@@ -48,7 +42,29 @@ int move_forward(server_t *server, response_t *response, request_t *request)
         client->data.y = server->params.height - 1;
     else if (client->data.y >= server->params.height)
         client->data.y = 0;
+}
+
+int move_forward(server_t *server, response_t *response, request_t *request)
+{
+    client_t *client = request->client;
+    map_t **map = server->map;
+    direction_offset_t offset;
+    int last_x = client->data.x;
+    int last_y = client->data.y;
+
+    if (!server || !response || !request || !client)
+        return ERROR;
+    set_offset(&offset, client);
+    client->data.x += offset.x;
+    client->data.y += offset.y;
+    if (map[last_x][last_y].players > 0)
+        map[last_x][last_y].players--;
+    check_map_bounds(server, client);
+    map[client->data.x][client->data.y].players++;
     sprintf(response->response, "ok");
+    response->client->data.is_busy = true;
+    response->client->data.action_end_time =
+        get_action_end_time(server, FORWARD_TIME);
     return SUCCESS;
 }
 
@@ -60,6 +76,9 @@ int rotate_right(server_t *server, response_t *response, request_t *request)
         return ERROR;
     client->data.direction = (client->data.direction + 1) % MAX_DIRECTION;
     sprintf(response->response, "ok");
+    response->client->data.is_busy = true;
+    response->client->data.action_end_time =
+        get_action_end_time(server, RIGHT_TIME);
     return SUCCESS;
 }
 
@@ -69,8 +88,11 @@ int rotate_left(server_t *server, response_t *response, request_t *request)
 
     if (!server || !response || !request || !client)
         return ERROR;
-    client->data.direction = (client->data.direction - 1
-        + MAX_DIRECTION) % MAX_DIRECTION;
+    client->data.direction = (client->data.direction - 1 + MAX_DIRECTION)
+        % MAX_DIRECTION;
     sprintf(response->response, "ok");
+    response->client->data.is_busy = true;
+    response->client->data.action_end_time =
+        get_action_end_time(server, LEFT_TIME);
     return SUCCESS;
 }
