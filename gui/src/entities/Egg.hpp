66f2a205c0fcbf1e60b81ef632d46a15a8e3ad27
Chassagne <robin.chassagne@epitech.eu>
2025-06-11 18:59:51 +0200

    feat: egg structure

---
+++ gui/src/entities/Egg.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+namespace gui {
+  struct Egg {
+    int id;
+    std::size_t x;
+    std::size_t y;
+    int layingPlayerId;
+    std::string teamName;
+
+    Egg(int id, std::size_t x, std::size_t y, int layingPlayerId, const std::string& teamName)
+      : id(id), x(x), y(y), layingPlayerId(layingPlayerId), teamName(teamName) {}
+  };
+}
