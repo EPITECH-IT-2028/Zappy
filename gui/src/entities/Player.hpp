0dae7ab16b361e9cfab8acb3c815cdfe24c6c23e
Chassagne <robin.chassagne@epitech.eu>
2025-06-12 13:40:25 +0200

    fix: change the orientation in an ENUM and change the level from int to uint8_t

--- gui/src/entities/Player.hpp
+++ gui/src/entities/Player.hpp
@@ -1,16 +1,24 @@
 #pragma once
+#include <cstdint>
 #include <string>
+#include <cstddef>
 
 namespace gui {
   struct Player {
     int id;
     std::size_t x;
     std::size_t y;
-    int orientation;
-    int level;
+    enum orientation {
+      NORTH = 0,
+      EAST = 1,
+      SOUTH = 2,
+      WEST = 3
+    };
+    orientation orient;
+    std::uint8_t level;
     std::string teamName;
 
     Player(int id, std::size_t x, std::size_t y, int orientation, int level, const std::string& teamName)
-      : id(id), x(x), y(y), orientation(orientation), level(level), teamName(teamName) {}
+      : id(id), x(x), y(y), orient(orient), level(level), teamName(teamName) {}
   };
 };
