db5dc412a3b30df1741d8563a55585510cec0ec0
Chassagne <robin.chassagne@epitech.eu>
2025-06-12 14:01:17 +0200

    fix: forgot to change the orientation type in the constructor (it was int, change to Orientation ENUM

--- gui/src/entities/Player.hpp
+++ gui/src/entities/Player.hpp
@@ -8,17 +8,18 @@ namespace gui {
     int id;
     std::size_t x;
     std::size_t y;
-    enum orientation {
+    enum Orientation {
       NORTH = 0,
       EAST = 1,
       SOUTH = 2,
       WEST = 3
     };
-    orientation orient;
+    Orientation orient;
     std::uint8_t level;
     std::string teamName;
 
-    Player(int id, std::size_t x, std::size_t y, int orientation, int level, const std::string& teamName)
-      : id(id), x(x), y(y), orient(orient), level(level), teamName(teamName) {}
+    Player(int id, std::size_t x, std::size_t y, Orientation orientation,
+      std::uint8_t level, const std::string& teamName) noexcept
+      : id(id), x(x), y(y), orient(orientation), level(level), teamName(teamName) {}
   };
 };
