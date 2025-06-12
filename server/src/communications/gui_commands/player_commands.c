7d0b0c45c6a689e5451db1de215fdfb9ec47f5ef
Arthur <arthur.guerinault@epitech.eu>
2025-06-12 11:47:56 +0200

    fix: add one to orientation, dead to graphic

--- server/src/communications/gui_commands/player_commands.c
+++ server/src/communications/gui_commands/player_commands.c
@@ -37,7 +37,7 @@ void player_position(server_t *server, int index, int id)
         id,
         client->data.x,
         client->data.y,
-        client->data.orientation);
+        client->data.orientation + 1);
     send_code(server->clients[index]->fd, response);
 }
 
--- server/src/game/food.c
+++ server/src/game/food.c
@@ -24,7 +24,8 @@ void remove_food(server_t *server)
     if (server->nfds == 1)
         return;
     for (int i = 1; i < server->nfds; i++) {
-        if (server->clients[i] == NULL || server->fds[i].fd == -1)
+        if (server->clients[i] == NULL || server->fds[i].fd == -1
+        || server->clients[i]->data.is_graphic)
             continue;
         if (server->clients[i]->data.team_name != NULL) {
             server->clients[i]->data.inventory.food--;
