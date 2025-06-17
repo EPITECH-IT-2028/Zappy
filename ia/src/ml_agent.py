41d1565406b8c3440195c8c64d26d5c086a481b6
Etiennelbre <146018978+Etiennelbre@users.noreply.github.com>
2025-06-17 14:26:32 +0200

    Ia/level 1 to 2 and global strategy (#95)
    * feat: global strategy and level 1 to level 2
    
    * feat: global strategy and level 1 to 2
    
    * rem: useless print

--- ia/src/ml_agent.py
+++ ia/src/ml_agent.py
@@ -2,6 +2,7 @@ import protocole
 import game
 import math
 import random
+import utils
 
 def get_position(index, total_cells) -> tuple[int, int]:
   if index == 0:
@@ -20,79 +21,86 @@ def get_position(index, total_cells) -> tuple[int, int]:
     distance += 1
   return (0, 0)
 
-def search_closest_food(client):
-  vision_data = client["last_look"]
-  closest_food = None
-  min_distance = float('inf')
+def search_closest_resource(client, needed_resources):
+    vision_data = client["last_look"]
+    closest_resource = None
+    min_distance = float('inf')
 
-  for index, cell in enumerate(vision_data):
-    if 'food' in cell:
-      x, y = get_position(index, len(vision_data))
-      distance = math.sqrt(x**2 + y**2)
-      if distance < min_distance:
-        min_distance = distance
-        closest_food = {
-          'x': x,
-          'y': y,
-        }
-  
-  return closest_food
+    for index, cell in enumerate(vision_data):
+        for resource_name in needed_resources.keys():
+            if resource_name in cell.lower():
+                x, y = get_position(index, len(vision_data))
+                real_distance = math.sqrt(x*x + y*y)
+                
+                if real_distance < min_distance:
+                    min_distance = real_distance
+                    closest_resource = {
+                        'x': x,
+                        'y': y,
+                        'real_distance': real_distance,
+                        'resource_name': resource_name,
+                        'content': cell
+                    }
+                break
+
+    return closest_resource
       
-def get_action(client):
-  closest_food = search_closest_food(client)
+def get_action(client, needed_resources):
+  closest_resources = search_closest_resource(client, needed_resources)
 
-  if closest_food is None:
-    return "Right", None
+  if closest_resources is None:
+    return utils.RIGHT, None
 
-  x, y = closest_food["x"], closest_food["y"]
+  x, y = closest_resources["x"], closest_resources["y"]
+  resources_name = closest_resources["resource_name"]
 
   if x == 0 and y == 0:
     client["move"]["consecutive_turns"] = 0
     client["move"]["forward"] = False
-    client["move"]["distance_to_food"] = -1
-    client["move"]["last_food"] = None
-    return "Take", "food"
+    client["move"]["distance_to_target"] = -1
+    client["move"]["last_target"] = None
+    return utils.TAKE, resources_name
   
   if x == 0 and y > 0:
     client["move"]["consecutive_turns"] = 0
     client["move"]["forward"] = True
-    return "Forward", None
+    return utils.FORWARD, None
   
-  if client["move"]["last_food"] == "right" and client["move"]["distance_to_food"] == 0:
+  if client["move"]["last_target"] == "right" and client["move"]["distance_to_target"] == 0:
     client["move"]["consecutive_turns"] += 1
-    client["move"]["distance_to_food"] = -1
-    return "Right", None
+    client["move"]["distance_to_target"] = -1
+    return utils.RIGHT, None
   
-  if client["move"]["last_food"] == "left" and client["move"]["distance_to_food"] == 0:
+  if client["move"]["last_target"] == "left" and client["move"]["distance_to_target"] == 0:
     client["move"]["consecutive_turns"] += 1
-    client["move"]["distance_to_food"] = -1
-    return "Left", None
+    client["move"]["distance_to_target"] = -1
+    return utils.LEFT, None
   
   if client["move"]["consecutive_turns"] >= 2:
     client["move"]["consecutive_turns"] = 0
     client["move"]["forward"] = True
-    return "Forward", None
+    return utils.FORWARD, None
   
   if x > 0:
-    client["move"]["last_food"] = "right"
+    client["move"]["last_target"] = "right"
     client["move"]["forward"] = True
-    client["move"]["distance_to_food"] = y - 1
-    return "Forward", None
+    client["move"]["distance_to_target"] = y - 1
+    return utils.FORWARD, None
   
   if x < 0:
-    client["move"]["last_food"] = "left"
+    client["move"]["last_target"] = "left"
     client["move"]["forward"] = True
-    client["move"]["distance_to_food"] = y - 1
-    return "Forward", None
+    client["move"]["distance_to_target"] = y - 1
+    return utils.FORWARD, None
   
   client["move"]["consecutive_turns"] = 0
   client["move"]["forward"] = True
   return None, None
  
-def get_food(client):
-  action, arg = get_action(client)
+def get_resources(client, needed_resources):
+  action, arg = get_action(client, needed_resources)
 
-  if action == "Take":
+  if action == utils.TAKE:
     protocole.execute_command(client, action, arg)
   else:
     protocole.execute_command(client, action, None)
@@ -122,63 +130,128 @@ def analyze_vision(client):
 
   return analyzed_vision
 
+def define_needed_resources(client):
+  needed = client.get("needed_resources", {})
+  inventory = client.get("inventory", {})
+  missing_resources = {}
+
+  valid_resources = ["linemate", "deraumere", "sibur", "mendiane", "phiras", "thystame"]
+
+  for resource, quantity in needed.items():
+    if inventory.get(resource, 0) < quantity and resource in valid_resources:
+      missing_resources[resource] = quantity - inventory.get(resource, 0)
+
+  return missing_resources
+
+def verify_incantation(client, current_cell, needed_resources):
+    required_players = client["needed_resources"].get("nb_players", 0)
+    count_players = current_cell.count("player")
+
+    if needed_resources or not current_cell or count_players < required_players:
+        client["incantation"] = False
+        return
+    
+    client["incantation"] = True
+
+def setting_up_incantation(client):
+    valid_resources = ["linemate", "deraumere", "sibur", "mendiane", "phiras", "thystame"]
+    dropped_resources = []
+
+    for resource in valid_resources:
+        quantity = client["inventory"].get(resource, 0)
+
+        if quantity > 0:
+            protocole.execute_command(client, utils.SET, resource)
+
+        if quantity == 0:
+            dropped_resources.append(resource)
+
+    if len(dropped_resources) == len(valid_resources):
+       return True
+    else:
+       return False  
+
 def strategy(client):
     vision_data = analyze_vision(client)
+    needed = define_needed_resources(client)
+    food_count = client["inventory"].get("food", 0)
+    current_cell = client["last_look"][0] if client["last_look"] else None
 
-    if vision_data["food"]:
-        get_food(client)
-    else:
-        client["move"]["consecutive_turns"] += 1
-        if client["move"]["consecutive_turns"] >= 3:
-            client["move"]["consecutive_turns"] = 0
-            client["move"]["forward"] = True
-            protocole.execute_command(client, "Forward", None)
+    if not client["incantation"]:
+        verify_incantation(client, current_cell, needed)
+
+    if client["incantation"]:
+        print("Incantation is possible, setting up resources...")
+        if setting_up_incantation(client):
+            protocole.execute_command(client, utils.INCANTATION, None)
+        protocole.execute_command(client, utils.INVENTORY, None)
+        return
+
+    if needed and current_cell:
+        action, arg = get_action(client, needed)
+        if action == utils.TAKE and arg in current_cell.lower():
+            protocole.execute_command(client, action, arg)
+            protocole.execute_command(client, utils.INVENTORY, None)
+            return
+      
+    if food_count < 10:
+        if vision_data["food"]:
+            get_resources(client, {"food": 1})
         else:
-            if random.random() < 0.7:
-                protocole.execute_command(client, "Right", None)
+            client["move"]["consecutive_turns"] += 1
+            if client["move"]["consecutive_turns"] >= 3:
+                client["move"]["consecutive_turns"] = 0
+                client["move"]["forward"] = True
+                protocole.execute_command(client, utils.FORWARD, None)
             else:
-                protocole.execute_command(client, "Forward", None)
-    
-    protocole.execute_command(client, "Inventory", None)
-  
-  # TO DO: Implement strategy
+                if random.random() < 0.7:
+                    protocole.execute_command(client, utils.RIGHT, None)
+                else:
+                    protocole.execute_command(client, utils.FORWARD, None)
 
-  # if (client["inventory"].get("food", 0) < 5):
-  #   focus on getting food until the food count is >= 10
-  # if (client["inventory"].get("food", 0) < 10):
-  #   focus on getting food jusqu'a ce que le nombre de nourriture soit >= 20 and resources if encountered
-  # else:
-  #   focus on other resources and food if encountered
-
-# def get_cell_symbol(cell_content):
-#         if 'food' in cell_content.lower():
-#             return 'F'
-#         elif cell_content.strip() == '':
-#             return '.'
-#         else:
-#             return 'X'
-
-# def print_vision_cone(vision_data):
-#     if not vision_data:
-#         return
-    
-#     current_cell = get_cell_symbol(vision_data[0])
-#     print(f"[{current_cell}]")
-    
-#     index = 1
-#     distance = 1
-    
-#     while index < len(vision_data):
-#         line_size = distance * 2 + 1
-#         line_end = min(index + line_size, len(vision_data))
-
-#         line_cells = vision_data[index:line_end]
-#         line_symbols = [get_cell_symbol(cell) for cell in line_cells]
+    elif food_count < 20:
+        if "food" in current_cell.lower():
+            protocole.execute_command(client, utils.TAKE, "food")
+            protocole.execute_command(client, utils.INVENTORY, None)
+            return
         
-#         spaces = " " * (4 - distance) if distance <= 4 else ""
+        if needed:
+            get_resources(client, needed)
+            protocole.execute_command(client, utils.INVENTORY, None)
+            return
         
-#         line_display = " ".join(line_symbols)
-#         print(f" {spaces}[{line_display}]")
-        
-#         index = line_end
-#         distance += 1
+        if vision_data["food"]:
+            get_resources(client, {"food": 1})
+        else:
+            client["move"]["consecutive_turns"] += 1
+            if client["move"]["consecutive_turns"] >= 3:
+                client["move"]["consecutive_turns"] = 0
+                client["move"]["forward"] = True
+                protocole.execute_command(client, utils.FORWARD, None)
+            else:
+                if random.random() < 0.7:
+                    protocole.execute_command(client, utils.RIGHT, None)
+                else:
+                    protocole.execute_command(client, utils.FORWARD, None)
+
+    else:
+        if "food" in current_cell.lower():
+            protocole.execute_command(client, utils.TAKE, "food")
+            protocole.execute_command(client, utils.INVENTORY, None)
+            return
+       
+        if needed:
+            get_resources(client, needed)
+        else:
+            client["move"]["consecutive_turns"] += 1
+            if client["move"]["consecutive_turns"] >= 3:
+                client["move"]["consecutive_turns"] = 0
+                client["move"]["forward"] = True
+                protocole.execute_command(client, utils.FORWARD, None)
+            else:
+                if random.random() < 0.7:
+                    protocole.execute_command(client, utils.RIGHT, None)
+                else:
+                    protocole.execute_command(client, utils.FORWARD, None)
+
+    protocole.execute_command(client, utils.INVENTORY, None)
