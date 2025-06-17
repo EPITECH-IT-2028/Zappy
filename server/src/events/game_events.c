52308322b2eafa4cfd9b6b43c991e3d2d2eb8623
Nolann Dubos <nolann.dubos@epitech.eu>
2025-06-17 20:37:07 +0200

    feat: pex, pdr, pdg event

--- server/src/events/game_events.c
+++ server/src/events/game_events.c
@@ -33,3 +33,62 @@ void send_pbc(server_t *server, client_t *client, const char *message)
         }
     }
 }
+
+void send_pex(server_t *server, client_t *client)
+{
+    char response[BUFFER_SIZE];
+
+    snprintf(response, BUFFER_SIZE, "pex #%d", client->data.id);
+    for (int i = 1; i < server->nfds; i++) {
+        if (server->clients[i] != NULL &&
+            server->clients[i]->data.is_graphic) {
+            send_code(server->clients[i]->fd, response);
+        }
+    }
+}
+
+/**
+ * @brief Sends a PDR (Player Drop Resource) message to all graphic clients
+ *
+ * This function formats a message indicating the player's ID and the resource
+ * ID that was dropped, then sends it to all clients that are marked as graphic.
+ *
+ * @param server Server instance containing client list
+ * @param client The client who dropped the resource
+ * @param resource_id The ID of the resource that was dropped
+ */
+void send_pdr(server_t *server, client_t *client, int resource_id)
+{
+    char response[BUFFER_SIZE];
+
+    snprintf(response, BUFFER_SIZE, "pdr #%d %d", client->data.id, resource_id);
+    for (int i = 1; i < server->nfds; i++) {
+        if (server->clients[i] != NULL &&
+            server->clients[i]->data.is_graphic) {
+            send_code(server->clients[i]->fd, response);
+        }
+    }
+}
+
+/**
+ * @brief Sends a PGT (Player Get Resource) message to all graphic clients
+ *
+ * This function formats a message indicating the player's ID and the resource
+ * ID that was obtained, then sends it to all clients that are marked as graphic.
+ *
+ * @param server Server instance containing client list
+ * @param client The client who obtained the resource
+ * @param resource_id The ID of the resource that was obtained
+ */
+void send_pgt(server_t *server, client_t *client, int resource_id)
+{
+    char response[BUFFER_SIZE];
+
+    snprintf(response, BUFFER_SIZE, "pgt #%d %d", client->data.id, resource_id);
+    for (int i = 1; i < server->nfds; i++) {
+        if (server->clients[i] != NULL &&
+            server->clients[i]->data.is_graphic) {
+            send_code(server->clients[i]->fd, response);
+        }
+    }
+}
